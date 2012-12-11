fbbbb466b4a33e42b923b26dda8c25630899ed7e
Marton Suranyi <marton.suranyi@gmail.com>
2012-12-11 10:44:33 +0100

    example added

---
+++ examples/all_job_runner.rb
@@ -0,0 +1,11 @@
+require 'jenkins-json-api'
+
+jenkins = Jenkins.new 'myjenkinshost:80'
+
+
+jenkins.get_all_jobs.each do |job|
+  if job.name =~ /myjobpattern/
+    puts job.name
+    job.build_async # sending request async, useful if there is many job
+  end
+end
